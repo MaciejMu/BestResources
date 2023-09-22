989f50b73930ca8ab51e913f2ea9d77185b25351
Maciej Muter <116006124+MaciejMu@users.noreply.github.com>
2023-09-23 00:43:37 +0200

    change navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,33 +1,54 @@
-import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 const Navbar = () => {
   return (
-    <nav className="flex-center fixed top-0 z-50 w-full border-b-2 border-black-200 py-7 bg-gray-900/90 text-white">
+    <nav className="flex flex-col flex-center fixed top-0 z-50 w-full border-b-2 border-black-200 py-7 bg-gray-900/90 text-white">
       <div className="flex-between w-full mx-auto max-w-screen-2xl px-6 xs:px-8 sm:px-16">
         <Link href={"/"} className="text-5xl">
           👨🏻‍🏫
         </Link>
-        <Image
-          src={"/hamburger-menu.svg"}
-          alt={"Hamburger menu"}
-          width={30}
-          height={30}
-          className="block md:hidden"
-        />
-        <ul className="flex-center gap-x-3 max-md:hidden md:gap-x-10">
-          <li className="body-text text-gradient_blue-purple font-bold">
-            <Link href={"/"} target="_blank">
-              BLalal
-            </Link>
-          </li>
-          <li className="body-text text-purple font-bold">
-            <Link href={"/"} target="_blank">
-              dupa
-            </Link>
-          </li>
-        </ul>
+        <DropdownMenu>
+          <DropdownMenuTrigger className="body-text text-gradient_blue-purple font-bold">
+            My Other Projects
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="rounded-none bg-gray-900/90 border-indigo-500/50 p-8">
+            <DropdownMenuItem className="cursor-pointer hover:text-white-800 py-4">
+              <Link
+                href={"https://portfolio-maciejmu.vercel.app/"}
+                target="_blank"
+              >
+                MyPortfolio
+              </Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem className="cursor-pointer hover:text-white-800 py-4">
+              <Link
+                href={"https://my-recipe-maciejmu.vercel.app/"}
+                target="_blank"
+              >
+                MyRecipe
+              </Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem className="cursor-pointer hover:text-white-800 py-4">
+              <Link href={"https://waiter-helper.vercel.app/"} target="_blank">
+                WaiterHelper
+              </Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem className="cursor-pointer hover:text-white-800 py-4">
+              <Link
+                href={"https://maciej-muter-slowko.netlify.app/"}
+                target="_blank"
+              >
+                Słówko
+              </Link>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
     </nav>
   );
